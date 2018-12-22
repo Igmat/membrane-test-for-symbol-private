5bc3976f395dd5a180d1191032dd4b8f612e59f7
Justin Ridgewell <jridgewell@google.com>
2018-12-21 20:15:06 -0500

    Revamp test suite
    This'll now test string fields, public symbols, and private symbols
    (both exposed before the set operation and after).
    
    Also, it tests closure based access, which is how the syntax-only
    private fields operate.

---
+++ tests/index.js
@@ -0,0 +1,49 @@
+const helpers = require('./__helpers');
+
+describe('Membranes', () => {
+    describe('Closure based access', () => {
+        const set = (Side, base, value) => {
+            Side.set(base, value);
+        };
+        const get = (Side, base) => {
+            return Side.get(base);
+        };
+
+        describe('string fields', () => {
+            helpers.suite(helpers.stringFields, set, get);
+        });
+
+        describe('public symbol fields', () => {
+            helpers.suite(helpers.symbolFields, set, get);
+        });
+
+        describe('private symbol fields', () => {
+            helpers.suite(helpers.privateSymbolFields, set, get);
+        });
+    });
+
+    describe('Reified key based access', () => {
+        const set = (Side, base, value) => {
+            base[Side.field] = value;
+        };
+        const get = (Side, base) => {
+            return base[Side.field];
+        };
+
+        describe('string fields', () => {
+            helpers.suite(helpers.stringFields, set, get);
+        });
+
+        describe('public symbol fields', () => {
+            helpers.suite(helpers.symbolFields, set, get);
+        });
+
+        describe('private symbol fields', () => {
+            helpers.suite(helpers.privateSymbolFields, set, get);
+        });
+
+        describe('private symbol fields (pre exposed)', () => {
+            helpers.suite(helpers.preExposedPrivateSymbolFields, set, get);
+        });
+    });
+});
